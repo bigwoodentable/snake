1f32d36cd5d77bccbea2549a6b54472ec83514ae
xukang hong <hong.xkang@gmail.com>
2022-09-23 00:55:39 +1200

    basics 2D snake completed

--- src/index.ts
+++ src/index.ts
@@ -19,11 +19,26 @@ export default class Canvas {
     this.columns = this.canvas.width / this.unitSize;
   }
 
-  drawElement(x: number, y: number, color: string = '#FFFFFF'): void {
+  drawElement(
+    x?: number,
+    y?: number,
+    color: string = '#FFFFFF',
+    body?: { x: number; y: number }[]
+  ): void {
     this.ctx.fillStyle = color;
+
+    if (body !== undefined) {
+      // ***
+      for (let i = 0; i < body.length; i++) {
+        this.ctx.fillRect(body[i].x, body[i].y, this.unitSize, this.unitSize);
+      }
+    }
+
     this.ctx.fillRect(x, y, this.unitSize, this.unitSize);
   }
 }
+
+// Seperate Food into another object
 class Snake extends Canvas {
   private x: number;
   private y: number;
@@ -31,6 +46,8 @@ class Snake extends Canvas {
   private yNext: number;
   private xFood?: number;
   private yFood?: number;
+  private total: number;
+  private body: { x: number; y: number }[];
 
   constructor(
     x: number = 0,
@@ -43,6 +60,8 @@ class Snake extends Canvas {
     this.y = y;
     this.xNext = this.unitSize * this.speedFactor;
     this.yNext = yNext;
+    this.body = [];
+    this.total = 0;
     this.drawElement(this.x, this.y);
     this.move();
     this.turn();
@@ -72,49 +91,69 @@ class Snake extends Canvas {
         default:
           break;
       }
-
-      clearInterval(this.interval);
-      this.move();
     });
   }
 
   move(xFood?: number, yFood?: number): void {
     // every 250 interval the code runs
+    this.foodLocation();
     this.interval = window.setInterval(() => {
-      this.ctx.clearRect(this.x, this.y, this.unitSize, this.unitSize);
-      // this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      // this.ctx.clearRect(this.x, this.y, this.unitSize, this.unitSize);
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.createFood();
       this.update();
       this.hasEaten();
-      this.drawElement(this.x, this.y);
+      this.drawElement(this.x, this.y, undefined, this.body);
     }, 250);
   }
 
   update(): void {
+    this.die();
+
+    for (let i = 0; i < this.body.length - 1; i++) {
+      this.body[i] = this.body[i + 1];
+    }
+
+    this.body[this.total - 1] = { x: this.x, y: this.y };
+
     this.x += this.xNext;
     this.y += this.yNext;
 
-    if (this.x === this.canvas.width) {
+    if (this.x > this.canvas.width) {
       this.x = 0;
-    } else if (this.x === 0) {
+    } else if (this.x < 0) {
       this.x = this.canvas.width;
     }
-    if (this.y === this.canvas.height) {
+    if (this.y > this.canvas.height) {
       this.y = 0;
-    } else if (this.y === 0) {
+    } else if (this.y < 0) {
       this.y = this.canvas.height;
     }
   }
 
-  createFood() {
+  foodLocation() {
     this.xFood = randomNumb(this.canvas.width, this.unitSize);
     // change to this.canvas.height, using canvas.width for now as canvas is a square
     this.yFood = randomNumb(this.canvas.width, this.unitSize);
-    this.drawElement(this.xFood, this.yFood);
+  }
+
+  createFood() {
+    this.drawElement(this.xFood, this.yFood, 'red');
   }
 
   hasEaten(): void {
     if (this.xFood === this.x && this.yFood === this.y) {
+      this.total += 1;
+      this.foodLocation();
       this.createFood();
+    }
+  }
+
+  die() {
+    for (let i = 0; i < this.body.length - 1; i++) {
+      if (this.body[i].x === this.x && this.body[i].y === this.y) {
+        console.log('DEAD');
+      }
     }
   }
 }
